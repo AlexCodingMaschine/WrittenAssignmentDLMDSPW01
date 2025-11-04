78fe002e0a33f89ab1c66229782233b78f9dff4e
Alex <alexandergergert29@gmail.com>
2025-11-04 21:14:55 +0100

    Forgot to descirbe the Commit before: New Database with the ideal functions and planning for the main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+#This program is for the main part of the task
+#To use the databases created in database.py
+#Find the best 4 fit functions
+#Load test.csv
+#Test against the last 4 functions
+#Show and view the plots with bokeh
